db3c625a8be70b8b8f3b21bcbbcc609a9152074a
Vlad Kokhan <vvlad0202@gmail.com>
2020-07-10 12:41:02 +0400

    push to repo

---
+++ src/components/Navigation/Drawer/Drawer.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import classes from './Drawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+
+class Drawer extends Component {
+  renderLinks = (links) => {
+    return links.map((link, index) => {
+      return (
+        <li key={index}>
+          <NavLink
+            to={link.to}
+            exact={link.exact}
+            activeClassName={classes.active}
+            onClick={this.props.onToggle}
+          >
+            {link.label}
+          </NavLink>
+        </li>
+      );
+    });
+  };
+
+  render() {
+    const cls = [classes.Drawer];
+    if (!this.props.isOpen) cls.push(classes.close);
+
+    const links = [{ to: '/', label: 'Список', exact: true }];
+
+    if (this.props.isAuthenticated) {
+      links.push({ to: '/quiz-creator', label: 'Создать тест', exact: false });
+      links.push({ to: '/logout', label: 'Выйти', exact: false });
+    } else {
+      links.push({ to: '/auth', label: 'Авторизация', exact: false });
+    }
+
+    return (
+      <React.Fragment>
+        <nav className={cls.join(' ')}>
+          <ul>{this.renderLinks(links)}</ul>
+        </nav>
+        {this.props.isOpen ? <Backdrop onToggle={this.props.onToggle} /> : null}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Drawer;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducers from './store/reducers/rootReducer';
+import thunk from 'redux-thunk';
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const store = createStore(
+  rootReducers,
+  composeEnhancers(applyMiddleware(thunk)),
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
